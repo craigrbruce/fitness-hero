eca754e024344501b8a5234b3c08fbeb8d4ca25c
craig bruce <craig@craig-bruce.com>
2017-01-04 20:28:28 +1100

    hook ef to postgresql

--- server/Startup.cs
+++ server/Startup.cs
@@ -41,7 +41,7 @@ namespace Server
             // https://docs.efproject.net/en/latest/platforms/aspnetcore/new-db.html
             services.AddDbContext<DatabaseContext>(options =>
             {
-                options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection"));
+                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
             });
 
             services.AddIdentity<User, IdentityRole>()
