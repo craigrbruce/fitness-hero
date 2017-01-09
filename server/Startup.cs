5b184559dac5d875b1d8ee4b3160b092f6297371
craig bruce <craig@craig-bruce.com>
2017-01-09 21:06:16 +1100

    configure in-mem db if app is in test environment

--- server/Startup.cs
+++ server/Startup.cs
@@ -15,96 +15,91 @@ using Server.Models;
 
 namespace Server
 {
-    public class Startup
+  public class Startup
+  {
+    private readonly IHostingEnvironment _environment;
+    public Startup(IHostingEnvironment env)
     {
-        // Load application settings from JSON file(s)
-        // https://docs.asp.net/en/latest/fundamentals/configuration.html
-        public Startup(IHostingEnvironment env)
-        {
-            Configuration = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile($"appsettings.json", optional: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
-                .AddEnvironmentVariables()
-                .Build();
-        }
-
-        public IConfiguration Configuration { get; set; }
-
-        // Configure IoC container
-        // https://docs.asp.net/en/latest/fundamentals/dependency-injection.html
-        public void ConfigureServices(IServiceCollection services)
-        {
-            // https://docs.asp.net/en/latest/security/anti-request-forgery.html
-            services.AddAntiforgery(options => options.CookieName =  options.HeaderName = "X-XSRF-TOKEN");
-
-            // Register Entity Framework database context
-            // https://docs.efproject.net/en/latest/platforms/aspnetcore/new-db.html
-            services.AddDbContext<DatabaseContext>(options =>
-            {
-                options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddIdentity<User, IdentityRole>()
-                .AddEntityFrameworkStores<DatabaseContext>()
-                .AddDefaultTokenProviders();
-
-            services.AddMvcCore()
-                .AddAuthorization()
-                .AddViews()
-                .AddRazorViewEngine()
-                .AddJsonFormatters();
-        }
-
-        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
-        {
-            // Configure logging
-            // https://docs.asp.net/en/latest/fundamentals/logging.html
-            factory.AddConsole(Configuration.GetSection("Logging"));
-            factory.AddDebug();
-
-            // Serve static files
-            // https://docs.asp.net/en/latest/fundamentals/static-files.html
-            app.UseStaticFiles();
-
-            // Enable external authentication provider(s)
-            // https://docs.asp.net/en/latest/security/authentication/sociallogins.html
-            app.UseIdentity();
-
-            if (!string.IsNullOrEmpty(Configuration["Authentication:Facebook:AppId"]))
-            {
-                app.UseFacebookAuthentication(new FacebookOptions
-                {
-                    AppId = Configuration["Authentication:Facebook:AppId"],
-                    AppSecret = Configuration["Authentication:Facebook:AppSecret"],
-                    Scope = { "email" },
-                    Fields = { "name", "email" },
-                    SaveTokens = true,
-                });
-            }
-
-            // Configure ASP.NET MVC
-            // https://docs.asp.net/en/latest/mvc/index.html
-            app.UseMvc(routes =>
-            {
-                routes.MapRoute("default", "{*url}", new { controller = "Home", action = "Index" });
-            });
-        }
-
-        public static void Main()
-        {
-            var cwd = Directory.GetCurrentDirectory();
-            var web = Path.GetFileName(cwd) == "server" ? "../public" : "public";
-
-            var host = new WebHostBuilder()
-                .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseWebRoot(web)
-                .UseKestrel()
-                .UseIISIntegration()
-                .UseStartup<Startup>()
-                .Build();
-
-            host.Run();
-        }
+      _environment = env;
+      Configuration = new ConfigurationBuilder()
+          .SetBasePath(env.ContentRootPath)
+          .AddJsonFile($"appsettings.json", optional: true)
+          .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+          .AddEnvironmentVariables()
+          .Build();
     }
+
+    public IConfiguration Configuration { get; set; }
+
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddAntiforgery(options => options.CookieName = options.HeaderName = "X-XSRF-TOKEN");
+
+      if (_environment.IsEnvironment("Test"))
+      {
+        services.AddDbContext<DatabaseContext>(
+            options => options.UseSqlite("Data Source=:memory:"),
+            ServiceLifetime.Singleton
+            );
+      }
+      else
+      {
+        services.AddDbContext<DatabaseContext>(options =>
+        {
+          options.UseNpgsql(Configuration.GetConnectionString("DefaultConnection"));
+        });
+
+        services.AddIdentity<User, IdentityRole>()
+            .AddEntityFrameworkStores<DatabaseContext>()
+            .AddDefaultTokenProviders();
+      }
+
+      services.AddMvcCore()
+          .AddAuthorization()
+          .AddViews()
+          .AddRazorViewEngine()
+          .AddJsonFormatters();
+    }
+
+    public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory factory)
+    {
+      factory.AddConsole(Configuration.GetSection("Logging"));
+      factory.AddDebug();
+
+      app.UseStaticFiles();
+      app.UseIdentity();
+
+      if (!string.IsNullOrEmpty(Configuration["Authentication:Facebook:AppId"]))
+      {
+        app.UseFacebookAuthentication(new FacebookOptions
+        {
+          AppId = Configuration["Authentication:Facebook:AppId"],
+          AppSecret = Configuration["Authentication:Facebook:AppSecret"],
+          Scope = { "email" },
+          Fields = { "name", "email" },
+          SaveTokens = true,
+        });
+      }
+
+      app.UseMvc(routes =>
+      {
+        routes.MapRoute("default", "{*url}", new { controller = "Home", action = "Index" });
+      });
+    }
+    public static void Main()
+    {
+      var cwd = Directory.GetCurrentDirectory();
+      var web = Path.GetFileName(cwd) == "server" ? "../public" : "public";
+
+      var host = new WebHostBuilder()
+          .UseContentRoot(Directory.GetCurrentDirectory())
+          .UseWebRoot(web)
+          .UseKestrel()
+          .UseIISIntegration()
+          .UseStartup<Startup>()
+          .Build();
+
+      host.Run();
+    }
+  }
 }
