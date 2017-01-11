6d23ac4b757264362814a2714ef3206ecf8a6395
craig bruce <craig@craig-bruce.com>
2017-01-11 20:01:44 +1100

    further flesh out client controller

--- server/Controllers/Api/Clients/ClientsController.cs
+++ server/Controllers/Api/Clients/ClientsController.cs
@@ -12,16 +12,52 @@ namespace Server.Controllers.Api.Clients
     private readonly IRepository<Client> _repository;
     public ClientsController(IRepository<Client> repository)
     {
-        _repository = repository;
+      _repository = repository;
     }
 
-    // api/v1/users/me
     [HttpGet()]
     public IActionResult Get()
     {
-        // example code: we may need a common Response object with pagination or whatever we decide on
-        var clients =  _repository.GetList();
-        return Json(clients);
+      // example code: we may need a common Response object with pagination or whatever we decide on
+      var clients = _repository.GetList();
+      return Json(clients);
+    }
+
+    [HttpGet("{id}")]
+    public IActionResult Get(int id)
+    {
+      var result = _repository.Get(id);
+      if (result == null)
+      {
+        return NotFound($"Client with id '{id}' not found");
+      }
+      return Json(result);
+    }
+
+    [HttpPost]
+    public IActionResult Post([FromBody]Client client)
+    {
+      var saved = _repository.Save(client);
+      return Created($"{Request.Path.Value}/{saved.Id}", saved);
+    }
+
+    [HttpPut("{id}")]
+    public IActionResult Put(int id, [FromBody]Client client)
+    {
+      var existing = _repository.Get(id);
+      if (existing == null)
+      {
+        return NotFound();
+      }
+      var updated = _repository.Update(client);
+      return Json(updated);
+    }
+
+    [HttpDelete("{id}")]
+    public IActionResult Delete(int id)
+    {
+      _repository.Delete(new[] { id });
+      return NoContent();
     }
   }
 }
