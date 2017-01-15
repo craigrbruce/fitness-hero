c54204902881a38805d16375373d3cdf26086bd7
Craig Bruce <craig@craig-bruce.com>
2017-01-16 08:09:02 +1100

    add experimental generic api controller for basic CRUD actions

--- server/Controllers/Api/Clients/ClientsController.cs
+++ server/Controllers/Api/Clients/ClientsController.cs
@@ -1,4 +1,3 @@
-using System;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Server.Models;
@@ -6,66 +5,12 @@ using Server.Persistence;
 
 namespace Server.Controllers.Api.Clients
 {
-  [Authorize]
-  [Route("api/v1/[controller]")]
-  public class ClientsController : Controller
-  {
-    private readonly IRepository<Client> _repository;
-    public ClientsController(IRepository<Client> repository)
+    [Authorize]
+    [Route("api/v1/[controller]")]
+    public class ClientsController : ApiController<Client>
     {
-      _repository = repository;
+        public ClientsController(IRepository<Client> repository) : base(repository)
+        {
+        }
     }
-
-    [HttpGet()]
-    public IActionResult Get()
-    {
-      // example code: we may need a common Response object with pagination or whatever we decide on
-      var clients = _repository.GetList();
-      return Json(clients);
-    }
-
-    [HttpGet("{id}")]
-    public IActionResult Get(int id)
-    {
-      var result = _repository.Get(id);
-      if (result == null)
-      {
-        return NotFound($"Client with id '{id}' not found");
-      }
-      return Json(result);
-    }
-
-    [HttpPost]
-    public IActionResult Post([FromBody]Client client)
-    {
-      var saved = _repository.Save(client);
-      return Created($"{Request.Path.Value}/{saved.Id}", saved);
-    }
-
-    [HttpPut("{id}")]
-    public IActionResult Put(int id, [FromBody]Client client)
-    {
-      var existing = _repository.Get(id);
-      if (existing == null)
-      {
-        return NotFound();
-      }
-      var updated = _repository.Update(client);
-      return Json(updated);
-    }
-
-    [HttpDelete("{id}")]
-    public IActionResult Delete(int id)
-    {
-      _repository.Delete(new[] { id });
-      return NoContent();
-    }
-
-    [HttpDelete]
-    public IActionResult BulkDelete(int[] ids)
-    {
-      _repository.Delete(ids);
-      return NoContent();
-    }
-  }
 }
