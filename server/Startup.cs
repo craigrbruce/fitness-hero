424ce29655f378f56da0ca650a046ea67e2d6249
Nick Carroll <nlcarroll@gmail.com>
2017-01-16 23:32:06 +1100

    Missing using statement in Startup.cs

--- server/Startup.cs
+++ server/Startup.cs
@@ -6,6 +6,7 @@ using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
+using Server.Models.Client;
 using Server.Models;
 using Server.Persistence;
 
