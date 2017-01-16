9dea846a876ab02c4b76971cbf89e4c8822275b3
Craig Bruce <craig@craig-bruce.com>
2017-01-16 15:19:59 +1100

    PR feedback

--- server/Models/Client/Client.cs
+++ server/Models/Client/Client.cs
@@ -1,7 +1,5 @@
 using System;
-using System.Collections.Generic;
 using Server.Models.Core;
-using Server.Models.Schedule;
 
 namespace Server.Models.Client
 {
@@ -11,16 +9,13 @@ namespace Server.Models.Client
     public string GivenName { get; set; }
     public string FamilyName { get; set; }
     public string Email { get; set; }
-    public float Weight { get; set; }
+    public float StartingWeight { get; set; }
     public float Height { get; set; }
     public DateTime DateOfBirth { get; set; }
     public string TelephoneNumber { get; set; }
     public string EmergencyContactTelephoneNumber { get; set; }
     public string EmergencyContactName { get; set; }
     public Image Avatar { get; set; }
-    public ICollection<Image> ProgressPhotos { get; set; } = new List<Image>();
-    public ICollection<Injury> Injuries { get; set; } = new List<Injury>();
-    public SchedulePreference SchedulePreference {get; set;} = SchedulePreference.Any;
 
   }
 }
