5cad7bddf0fb9526c001ddb014962cfd1156e204
Craig Bruce <craig@craig-bruce.com>
2017-01-16 15:21:58 +1100

    add injuries back to client

--- server/Models/Client/Client.cs
+++ server/Models/Client/Client.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Collections.Generic;
 using Server.Models.Core;
 
 namespace Server.Models.Client
@@ -9,13 +10,14 @@ namespace Server.Models.Client
     public string GivenName { get; set; }
     public string FamilyName { get; set; }
     public string Email { get; set; }
-    public float StartingWeight { get; set; }
+    public float Weight { get; set; }
     public float Height { get; set; }
     public DateTime DateOfBirth { get; set; }
     public string TelephoneNumber { get; set; }
     public string EmergencyContactTelephoneNumber { get; set; }
     public string EmergencyContactName { get; set; }
     public Image Avatar { get; set; }
+    public ICollection<Injury> Injuries { get; set; } = new List<Injury>();
 
   }
 }
