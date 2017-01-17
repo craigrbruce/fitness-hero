ff703c32f8fd87c566e97f1d419614d8a37c26f0
craig bruce <craig@craig-bruce.com>
2017-01-17 21:24:19 +1100

    remove 3rd party grid references to fix build

--- client/components/Clients/Clients.js
+++ client/components/Clients/Clients.js
@@ -1,35 +1,27 @@
-import 'fixed-data-table/dist/fixed-data-table.css';
 import React, { PropTypes } from 'react';
-import { Table, Cell, Column } from 'fixed-data-table';
 
 const title = 'Fitness Hero';
-
-const data = [
-  { name: 'client a' },
-  { name: 'client b' },
-  { name: 'client c' },
-  { name: 'client d' },
-  { name: 'client e' },
-  { name: 'client f' },
-  { name: 'client g' },
-  { name: 'client h' },
-  { name: 'client i' },
-];
-
-const TextCell = ({
-  rowIndex,
-  data,
-  col,
-  ...props
-}) => (
-  <Cell {...props}>
-    {data[rowIndex][col]}
-  </Cell>
-);
 
 class Home extends React.Component {
   static propTypes = {
   };
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [
+        { name: 'client a' },
+        { name: 'client b' },
+        { name: 'client c' },
+        { name: 'client d' },
+        { name: 'client e' },
+        { name: 'client f' },
+        { name: 'client g' },
+        { name: 'client h' },
+        { name: 'client i' },
+      ],
+    };
+  }
 
   componentDidMount() {
     document.title = title;
@@ -38,21 +30,7 @@ class Home extends React.Component {
   render() {
     return (
       <div style={{ width: '100%', height: '100%' }}>
-        <Table
-          height={5000}
-          width={5000}
-          headerHeight={50}
-          rowHeight={50}
-          rowsCount={data.length}
-        >
-          <Column
-            width={1000}
-            header={<Cell>Name</Cell>}
-            cell={
-              <TextCell data={data} width={50} />
-            }
-          />
-        </Table>
+        some sort of client grid here
       </div>
     );
   }
