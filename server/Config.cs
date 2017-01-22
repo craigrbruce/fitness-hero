23fd47aae7b18a005ed4727a119c7ddf6342692b
Nick Carroll <nlcarroll@gmail.com>
2017-01-22 22:52:58 +1100

    Add hybrid grant type config for clients

--- server/Config.cs
+++ server/Config.cs
@@ -1,6 +1,3 @@
-// Copyright (c) Brock Allen & Dominick Baier. All rights reserved.
-// Licensed under the Apache License, Version 2.0. See LICENSE in the project root for license information.
-
 using IdentityServer4;
 using IdentityServer4.Models;
 using System.Collections.Generic;
@@ -9,7 +6,6 @@ namespace Server
 {
     public class Config
     {
-        // scopes define the resources in your system
         public static IEnumerable<IdentityResource> GetIdentityResources()
         {
             return new List<IdentityResource>
@@ -27,11 +23,11 @@ namespace Server
             };
         }
 
-        // clients want to access resources (aka scopes)
         public static IEnumerable<Client> GetClients()
         {
             return new List<Client>
             {
+                // Refer to: https://github.com/IdentityServer/IdentityServer4.Samples/tree/dev/Quickstarts/7_JavaScriptClient
                 new Client
                 {
                     ClientId = "web",
@@ -48,6 +44,32 @@ namespace Server
                         IdentityServerConstants.StandardScopes.Profile,
                         "api"
                     }
+                },
+
+                // Refer to: https://github.com/IdentityServer/IdentityServer4.Samples/tree/dev/Quickstarts/6_AspNetIdentity/src/MvcClient
+                new Client
+                {
+                    ClientId = "mvc",
+                    ClientName = "MVC Client",
+                    AllowedGrantTypes = GrantTypes.HybridAndClientCredentials,
+
+                    RequireConsent = true,
+
+                    ClientSecrets = 
+                    {
+                        new Secret("secret".Sha256())
+                    },
+
+                    RedirectUris = { "http://localhost:5000/signin-oidc" },
+                    PostLogoutRedirectUris = { "http://localhost:5000" },
+
+                    AllowedScopes =
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "api"
+                    },
+                    AllowOfflineAccess = true
                 }
             };
         }
