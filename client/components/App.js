4c30d55521d452a743cf4fd0fadca1f74399a9c3
Craig Bruce <craig@craig-bruce.com>
2017-01-24 13:09:48 +1100

    use index as a landing page if not logged in or SPA page if logged in

--- client/components/App.js
+++ client/components/App.js
@@ -2,7 +2,6 @@ import React from 'react';
 import { connect } from 'react-redux';
 import * as Mdl from 'react-mdl';
 import s from './App.css';
-import LandingPage from './LandingPage';
 import * as api from '../lib/api';
 
 class App extends React.Component {
@@ -15,47 +14,45 @@ class App extends React.Component {
 
   render() {
     return (
-      !window.userName ?
-        <LandingPage /> :
-        <Mdl.Layout fixedDrawer fixedHeader >
-          <Mdl.Header title="Fitness Hero" className={s.header} >
-            <div style={{ position: 'relative' }}>
-              <Mdl.Button
-                primary
-                id="user-menu"
-                style={{ color: 'white' }}
-              >
-                {
-                  `Welcome ${window.userName}`
-                }
-              </Mdl.Button>
-              <Mdl.Menu target="user-menu" align="right">
-                <Mdl.MenuItem ><a href="account/logout">Sign out</a>
-                </Mdl.MenuItem>
-              </Mdl.Menu>
-            </div>
-          </Mdl.Header>
-          <Mdl.Drawer
-            title="Fitness Hero" className={s.drawer}
-          >
-            <Mdl.Navigation className={s.navigation} >
-              <a href="#/">Home</a>
-              <a href="#/clients">Clients</a>
-              <a href="#/appointments">Appointments</a>
-            </Mdl.Navigation>
-          </Mdl.Drawer >
-          <Mdl.Content >
-            {
-              React.Children.map(
-                this.props.children,
-                (child) =>
-                  React.cloneElement(
-                    child, {
-                    }
-                  ))
-            }
-          </Mdl.Content>
-        </Mdl.Layout >
+      <Mdl.Layout fixedDrawer fixedHeader >
+        <Mdl.Header title="Fitness Hero" className={s.header} >
+          <div style={{ position: 'relative' }}>
+            <Mdl.Button
+              primary
+              id="user-menu"
+              style={{ color: 'white' }}
+            >
+              {
+                `Welcome ${window.userName}`
+              }
+            </Mdl.Button>
+            <Mdl.Menu target="user-menu" align="right">
+              <Mdl.MenuItem ><a href="account/logout">Sign out</a>
+              </Mdl.MenuItem>
+            </Mdl.Menu>
+          </div>
+        </Mdl.Header>
+        <Mdl.Drawer
+          title="Fitness Hero" className={s.drawer}
+        >
+          <Mdl.Navigation className={s.navigation} >
+            <a href="#/">Home</a>
+            <a href="#/clients">Clients</a>
+            <a href="#/appointments">Appointments</a>
+          </Mdl.Navigation>
+        </Mdl.Drawer >
+        <Mdl.Content >
+          {
+            React.Children.map(
+              this.props.children,
+              (child) =>
+                React.cloneElement(
+                  child, {
+                  }
+                ))
+          }
+        </Mdl.Content>
+      </Mdl.Layout >
     );
   }
 }
