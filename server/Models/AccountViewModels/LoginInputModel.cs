3505ae875259591f88e90fdafe0a30b66c554b40
Nick Carroll <nlcarroll@gmail.com>
2017-01-26 22:21:52 +1100

    Fixed missing label on Remember Me checkbox.

--- server/Models/AccountViewModels/LoginInputModel.cs
+++ server/Models/AccountViewModels/LoginInputModel.cs
@@ -5,10 +5,18 @@ namespace Server.Models.AccountViewModels
     public class LoginInputModel
     {
         [Required]
+        [EmailAddress]
+        [Display(Name = "Email")]
         public string Email { get; set; }
+
         [Required]
+        [DataType(DataType.Password)]
+        [Display(Name = "Password")]
         public string Password { get; set; }
+
+        [Display(Name = "Stay signed in")]
         public bool RememberLogin { get; set; }
+
         public string ReturnUrl { get; set; }
     }
 }
