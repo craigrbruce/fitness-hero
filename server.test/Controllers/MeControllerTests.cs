c71d04f24d081c094be8822b2f506fef45a5a7df
craig bruce <craig@craig-bruce.com>
2017-01-29 20:56:40 +1100

    start unit testing me controller, hit wall, will make a call on whether such testing is useful

--- server.test/Controllers/MeControllerTests.cs
+++ server.test/Controllers/MeControllerTests.cs
@@ -10,30 +10,32 @@ using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Logging;
 using Microsoft.AspNetCore.Builder;
+using System.Security.Claims;
+using System.Threading.Tasks;
 
 namespace Server.Test.Controllers.Api.Clients
 {
   public class MeControllerTests : ControllerTestsBase<Model>
   {
     private Mock<UserManager<User>> UserManager;
+    private Mock<IUserStore<User>> _userStore = new Mock<IUserStore<User>>();
     public MeControllerTests()
     {
-      var userStore = new Mock<IUserStore<User>>();
       var options = new Mock<IOptions<IdentityOptions>>();
       var hasher = new Mock<IPasswordHasher<User>>();
       var lookup = new Mock<ILookupNormalizer>();
       var provider = new Mock<System.IServiceProvider>();
       var logger = new Mock<ILogger<UserManager<User>>>();
-
       var userManager = new UserManager<User>(
-          userStore.Object, options.Object,
-          hasher.Object,
-          new List<IUserValidator<User>>(),
-          new List<IPasswordValidator<User>>(),
-          lookup.Object,
-          new IdentityErrorDescriber(),
-          provider.Object,
-          logger.Object
+            _userStore.Object,
+            options.Object,
+            hasher.Object,
+            new List<IUserValidator<User>>(),
+            new List<IPasswordValidator<User>>(),
+            lookup.Object,
+            new IdentityErrorDescriber(),
+            provider.Object,
+            logger.Object
           );
 
       Controller = new MeController(userManager);
@@ -42,8 +44,20 @@ namespace Server.Test.Controllers.Api.Clients
     [Fact]
     public void should_return_forbid_result_if_user_not_found()
     {
-      var result = (Controller as MeController)?.Get().ConfigureAwait(false);
+      var result = (Controller as MeController)?.Get().Result;
       result.Should().BeOfType<ForbidResult>();
+    }
+
+    [Fact(Skip="Mocking UserManager is prohibitively hard, make a call as to the usefulness of testing this")]
+    public void should_return_user_view_model_if_user_found()
+    {
+      _userStore
+        .Setup(
+                x => (x as UserManager<User>)
+                .GetUserAsync(It.IsAny<ClaimsPrincipal>()))
+                .Returns(new Task<User>(() => new User { Email = "some email" }));
+
+
     }
   }
 }
