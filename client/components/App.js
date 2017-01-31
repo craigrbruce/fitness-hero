74551231dc357963342ce1d18540a3e79e5cdeea
craig bruce <craig@craig-bruce.com>
2017-01-31 21:47:36 +1100

    change clients menu item icon

--- client/components/App.js
+++ client/components/App.js
@@ -39,7 +39,7 @@ class App extends React.Component {
                 <Mdl.ListItemContent icon="dashboard"><a href="/">Dashboard</a></Mdl.ListItemContent>
               </Mdl.ListItem>
               <Mdl.ListItem>
-                <Mdl.ListItemContent icon="person"><a href="clients">Clients</a></Mdl.ListItemContent>
+                <Mdl.ListItemContent icon="group"><a href="clients">Clients</a></Mdl.ListItemContent>
               </Mdl.ListItem>
               <Mdl.ListItem>
                 <Mdl.ListItemContent href="schedule" icon="date_range">Schedule</Mdl.ListItemContent>
