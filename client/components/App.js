cbf08530e5bf45b3ea13dd625a28f063522adfa3
craig bruce <craig@craig-bruce.com>
2017-02-01 13:27:08 +1100

    add menu items and icons to drawer

--- client/components/App.js
+++ client/components/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import ArrowDropDown from 'material-ui/svg-icons/navigation/arrow-drop-down';
+import Dashboard from 'material-ui/svg-icons/action/dashboard';
+import Event from 'material-ui/svg-icons/action/event';
+import Settings from 'material-ui/svg-icons/action/settings';
+import People from 'material-ui/svg-icons/social/people';
+import Person from 'material-ui/svg-icons/social/person';
 import { connect } from 'react-redux';
 import * as Mui from 'material-ui';
 import { getMe } from 'actions/account';
@@ -31,9 +36,9 @@ export class App extends React.Component {
 
   closeDrawer = () => this.setState({ open: false });
 
-  handleDrawerRequestChange = (open: boolean) => this.setState({ open });
+  handleDrawerRequestChange = (open) => this.setState({ open });
 
-  handleMenuTouchTap = (route: string) => {
+  handleMenuTouchTap = (route) => {
     history.push(route);
     setTimeout(() => this.setState({ open: false }), 1000);
   };
@@ -72,6 +77,14 @@ export class App extends React.Component {
           width={320}
           onRequestChange={this.handleDrawerRequestChange}
           >
+          <Mui.Menu>
+            <Mui.MenuItem primaryText="Dashboard" leftIcon={<Dashboard />} />
+            <Mui.MenuItem primaryText="Clients" leftIcon={<People />} />
+            <Mui.MenuItem primaryText="Schedule" leftIcon={<Event />} />
+            <Mui.Divider />
+            <Mui.MenuItem primaryText="Settings" leftIcon={<Settings />} />
+            <Mui.MenuItem primaryText="Acccount" leftIcon={<Person />} />
+          </Mui.Menu>
         </Mui.Drawer >
         <div className="main-content">
           {
